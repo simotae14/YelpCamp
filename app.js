a6fb638c1ef6de78cd401fcb50dca1e61a8c1246
Simone Taeggi <startae14@gmail.com>
2017-03-18 10:50:03 +0000

    Aggiunta la root route

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.get("/", function(req, res){
+  res.send("Questa sarà presto la landing page");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("The YelpCamp Server Has Started!");
 });
