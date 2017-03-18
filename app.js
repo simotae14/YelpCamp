9c77f80252ad91c57e52ea6809e0e0cebd3f327a
Simone Taeggi <startae14@gmail.com>
2017-03-18 11:32:54 +0000

    creata route campgrounds con relativa view

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get("/", function(req, res){
   res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+  var campgrounds = [
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8358/8444469474_8f4b935818.jpg"},
+    {name: "Granite Hill", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"}
+  ];
+  res.render("campgrounds", { campgrounds: campgrounds});
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("The YelpCamp Server Has Started!");
 });
